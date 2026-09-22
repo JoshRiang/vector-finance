938b53fa0f1739b586bf2f01ba71ada578e12a55
Joshua Riangkamang <sandinya1sampai8@gmail.com>
2026-09-23 01:42:29 +0700

    VECTOR Suite: daily brief, local store, timezone fix, tests

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class _FinancePageState extends State<FinancePage> {
 
   Future<void> _bootstrap() async {
     final prefs = await SharedPreferences.getInstance();
-    final id = prefs.getString('vector.user_id') ?? 'local';
+    final id = prefs.getString('vector.user_id') ?? Api.defaultUserId;
     _api = Api(userId: id);
     await _refresh();
   }
